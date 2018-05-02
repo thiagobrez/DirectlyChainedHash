5de4bbfd1fa3b55b6c9a0f51268fc1fb975f0e11
Thiago Brezinski <thiagobrez@gmail.com>
2018-05-01 23:12:43 -0300

    Concluída lógica e implementados alguns testes manuais.

--- src/com/company/Hash.java
+++ src/com/company/Hash.java
@@ -2,39 +2,85 @@ package com.company;
 
 import java.util.Arrays;
 
+/**
+ * @author João Grasel Cariolato
+ * @author Thiago Brezinski
+ */
 public class Hash {
 
     private int[] dados;
-    private int[] proximosIndices;
+    private int[] prox;
     private int tamanho;
+    private int espalhamento;
 
-    public Hash(int totalElementos, int fatorCarga) {
+    public Hash(int totalElementos, int fatorCarga, int espalhamento) throws Exception {
         this.tamanho = totalElementos / fatorCarga;
-        this.dados = new int[this.tamanho * 2];
-        this.proximosIndices = new int[this.tamanho * 2];
-        Arrays.fill(this.dados, -1);
-        Arrays.fill(this.proximosIndices, -1);
+        if(espalhamento >= this.tamanho) throw new Exception("O espalhamento deve ser menor que o tamanho!");
+        this.espalhamento = espalhamento;
+        this.dados = new int[this.tamanho];
+        this.prox = new int[this.tamanho];
+        Arrays.fill(this.prox, -1);
+    }
+
+    public Hash(int tamanho, int espalhamento) throws Exception {
+        if(espalhamento >= tamanho) throw new Exception("O espalhamento deve ser menor que o tamanho!");
+        this.tamanho = tamanho;
+        this.espalhamento = espalhamento;
+        this.dados = new int[this.tamanho];
+        this.prox = new int[this.tamanho];
+        Arrays.fill(this.prox, -1);
     }
 
     private int hashID(int dado) {
-        return dado % this.tamanho;
+        return dado % this.espalhamento;
     }
 
-    //TODO: encadear posicoes livres no oveflow
-
-    public void inserir(int dado) {
+    public void inserir(int dado) throws Exception {
         int grupo = hashID(dado);
-        if(this.dados[grupo] == -1) {
-            this.dados[grupo] = dado;
-        } else {
-            if(this.proximosIndices[grupo] == -1) {
-                for(int i = this.tamanho; i < this.dados.length; i++) {
-                    if(this.dados[i] == -1) {
-                        this.dados[i] = dado;
-                        this.proximosIndices[grupo] = i;
-                    }
-                }
+        while(this.prox[grupo] != -1) {
+            if(grupo + this.espalhamento < this.tamanho - 1) {
+                grupo += this.espalhamento;
+            } else {
+                throw new Exception("Encadeamento cheio para o dado: " + dado + ".");
             }
+        }
+        this.dados[grupo] = dado;
+        this.prox[grupo] = grupo + this.espalhamento;
+    }
+
+    public boolean buscar(int dado) {
+        int grupo = hashID(dado);
+        if(this.dados[grupo] == dado) {
+            return this.prox[grupo] != -1;
+        }
+        while(this.prox[grupo] != -1) {
+            if(this.dados[this.prox[grupo]] == dado) {
+                return this.prox[this.prox[grupo]] != -1;
+            }
+            grupo = this.prox[grupo];
+        }
+        return false;
+    }
+
+    public void excluir(int dado) throws Exception {
+        int grupo = hashID(dado);
+        if(this.dados[grupo] == dado) {
+            this.prox[grupo] = -1;
+        }
+        while(this.prox[grupo] != -1) {
+            if(this.dados[this.prox[grupo]] == dado) {
+                this.prox[this.prox[grupo]] = -1;
+                return;
+            } else {
+                grupo = this.prox[grupo];
+            }
+        }
+        throw new Exception("Elemento inexistente!");
+    }
+
+    public void print() {
+        for(int i = 0; i < this.tamanho; i++) {
+            System.out.println(i + ":" + "[" + this.dados[i] + "]" + "[" + this.prox[i] + "]");
         }
     }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,58 @@ public class Main {
 
     public static void main(String[] args) {
 
+        try {
+            Hash hash1 = new Hash(10, 2);
+
+            hash1.inserir(10);
+            System.out.println("Inserido elemento 10");
+            hash1.inserir(11);
+            System.out.println("Inserido elemento 11");
+            hash1.inserir(12);
+            System.out.println("Inserido elemento 12");
+            hash1.inserir(13);
+            System.out.println("Inserido elemento 13");
+            hash1.inserir(14);
+            System.out.println("Inserido elemento 14");
+            hash1.inserir(15);
+            System.out.println("Inserido elemento 15");
+
+            System.out.println();
+
+            System.out.println("Hash atual: ");
+            hash1.print();
+            System.out.println();
+
+            System.out.println("Existe elemento 12: " + hash1.buscar(12));
+            System.out.println("Existe elemento 16: " + hash1.buscar(16));
+
+            System.out.println();
+            hash1.excluir(15);
+            System.out.println("Excluído elemento 15");
+            hash1.excluir(12);
+            System.out.println("Excluído elemento 12");
+            System.out.println();
+
+            System.out.println("Existe elemento 12: " + hash1.buscar(12));
+            System.out.println();
+
+            System.out.println("Hash atual: ");
+            hash1.print();
+            System.out.println();
+
+            hash1.inserir(18);
+            System.out.println("Inserido elemento 18");
+            System.out.println();
+
+            System.out.println("Hash atual: ");
+            hash1.print();
+            System.out.println();
+
+            System.out.println("Tentando excluir elemento 20...");
+            hash1.excluir(20);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
